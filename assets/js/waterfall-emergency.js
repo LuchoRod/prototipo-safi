171be36c98f657f2bbb57b2173d59517cf260ae6
Jose <holzent@gmail.com>
2019-04-24 19:07:41 -0500

    Cambios penultimos

--- assets/js/waterfall-emergency.js
+++ assets/js/waterfall-emergency.js
@@ -18,20 +18,20 @@ window.onload = function () {
             yValueFormatString: "#,##0.##",
             dataPoints: [
                 
-                { label: "Collique", y: 46.12, color:"#0E0DAE" },
-                { label: "SUAM", y: 2.00, color:"#18C60C" },
-                { label: "Juntoz", y: 17.50, color:"#18C60C" },
-                { label: "Comunal", y: 0.35, color: "#18C60C" },
+                { label: "Collique", y: 46.12, color:"#FC0000" },
+                { label: "SUAM", y: 2.00, color:"#E64F4F" },
+                { label: "Juntoz", y: 17.50, color:"#E64F4F" },
+                { label: "Comunal", y: 0.35, color: "#E64F4F" },
                 { label: "AP SM", y: -0.20, color:"#DFDDF7" },
                 { label: "TW", y: -19.10, color:"#DFDDF7" },
                 { label: "Hiro Sake", y: -0.35, color:"#DFDDF7" },
                 { label: "FIVIS I", y: -1.50, color:"#DFDDF7" },
                 { label: "FIVIS I", y: -2.00, color:"#DFDDF7" },
                 { label: "Total", y:-41.74, color:"#DFDDF7" },
-                { label: "Collique", y: 0.27, color:"#18C60C" },
+                { label: "Collique", y: 0.27, color:"#E64F4F" },
                 { label: "SUAM", y: -0.10, color:"#DFDDF7" },
                 { label: "Juntoz", y: -0.37, color:"#DFDDF7" },
-                { label: "Comunal", y: 0.34, color: "#0E0DAE" },
+                { label: "Comunal", y: 0.34, color: "#FC0000" },
             ]
         },
     ]
