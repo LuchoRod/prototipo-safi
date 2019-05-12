b60bffb8ea92ac2c7ef9888ceee1b8e184d4dbf2
Jose <holzent@gmail.com>
2019-05-12 12:34:24 -0500

    toggle tabla

---
+++ assets/js/noticias-component.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+     infinite: true,
+     slidesToShow: 3,
+     slidesToScroll: 3,
+     "nextArrow":"<div class='icon-arrow-right btnv' _ngcontent-c4 ></div>",
+     "prevArrow":"<div class='icon-arrow-left btnv' _ngcontent-c4 ></div>"
+    });
+  });
